aff4826f5a948d4bd3baffb9ba363c51f398e6b2
Sara-Hammad <152811008+Sara-Hammad@users.noreply.github.com>
2024-09-27 21:35:54 +0300

    Update Navbar.jsx

--- final project/finalProject/src/components/Navbar/Navbar.jsx
+++ final project/finalProject/src/components/Navbar/Navbar.jsx
@@ -1,34 +1,61 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import styleNavbar from './Navbar.module.css'
+import Login from "../Popups/Login";  
+import Register from "../Popups/Register";
+import styleNavbar from './Navbar.module.css';
 
 export default function Navbar() {
-    return (
-        <>
-            <div className={styleNavbar.navbar}>
-                <div className={styleNavbar.logo}>
-                    <h1>SHAGARA</h1>
-                </div>
+  const [isLoginOpen, setIsLoginOpen] = useState(false); 
+  const [isRegisterOpen, setIsRegisterOpen] = useState(false);
 
-                <div className={styleNavbar.links}>
-                        <Link to="/" className={styleNavbar.navElement}>Home</Link>
-                        <Link to="/about" className={styleNavbar.navElement}>About</Link>
-                        <Link  to="/trees" className={styleNavbar.navElement}>Trees</Link>
-                        <Link  to="/contact" className={styleNavbar.navElement}>Contact</Link>
-                    {/* <a className={styleNavbar.navElement} href="">Home</a>
-                    <a className={styleNavbar.navElement} href="">About</a>
-                    <a className={styleNavbar.navElement} href="">Trees</a>
-                    <a className={styleNavbar.navElement} href="">Contact</a> */}
-                    <div className={styleNavbar.profile}>
-                    <Link to="/" className={styleNavbar.navElement}>login/</Link>
-                    <Link to="/about" className={styleNavbar.navElement}>register</Link>
-                    </div>
-                </div>
+  const toggleLoginPopup = () => {
+    setIsLoginOpen(!isLoginOpen);
+  };
 
-            </div>
-        </>
-    )
+  const toggleRegisterPopup = () => {
+    setIsRegisterOpen(!isRegisterOpen);
+  };
+
+  return (
+    <>
+      <div className={styleNavbar.navbar}>
+        <div className={styleNavbar.logo}>
+          <h1>SHAGARA</h1>
+        </div>
+        <div className={styleNavbar.links}>
+          <Link to="/" className={styleNavbar.navElement}>Home</Link>
+          <Link to="/about" className={styleNavbar.navElement}>About</Link>
+          <Link to="/trees" className={styleNavbar.navElement}>Trees</Link>
+          <Link to="/contact" className={styleNavbar.navElement}>Contact</Link>
+
+          <div className={styleNavbar.profile}>
+            <button className={styleNavbar.enter} onClick={toggleLoginPopup}>Login</button>
+            <button className={styleNavbar.enter} onClick={toggleRegisterPopup}>Register</button>
+          </div>
+        </div>
+      </div>
+
+      
+      {isLoginOpen && (
+        <div className={styleNavbar.popup}>
+          <div className={styleNavbar.popupInner}>
+            <button className={styleNavbar.closeBtn} onClick={toggleLoginPopup}>X</button>
+            <Login />
+          </div>
+        </div>
+      )}
+
+      
+      {isRegisterOpen && (
+        <div className={styleNavbar.popup}>
+          <div className={styleNavbar.popupInner}>
+            <button className={styleNavbar.closeBtn} onClick={toggleRegisterPopup}>X</button>
+            <Register />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
-
